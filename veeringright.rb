58c38c1697d9d838a07e7e09893b63e545907d66
Humble <humbleaistudio@gmail.com>
2018-01-09 11:50:23 +0300

    update

--- veeringright.rb
+++ veeringright.rb
@@ -18,31 +18,41 @@ noteringsynth = [:b, :c, :d, :e, :f, :g]
 
 live_loop :drums1 do
   #stop
-  with_fx :vowel do
-    sample :bd_gas, amp: 1, release: 0.25, sustain: 0.25
-  end
   
   sample :bd_boom, amp: 2.5
+  
+  with_fx :echo, phase: 0.125, mix: 0.4 do
+    sample :bd_tek, delay: 0.25, amp: 0.25, sustain: 0, release: 0.1
+  end
+  
   sleep 0.5
+  
   
   sample :elec_hollow_kick, release: 0.25, sustain: 0.25, cutoff: rrand(20, 120), amp: 1, rate: [-1, 1].choose
+  sleep 0.25
   play noteringdrums.choose, release: 0.1, amp: 0.5
   
-  sleep 0.5
+  sleep 0.25
 end
 
 
 
 live_loop :fl do
   #stop
+  
+  sync :drums1
+  
   with_fx [:vowel, :ring_mod, :reverb].choose do
     sample :guit_e_slide, cutoff: rrand(20, 120), release: 0.05, amp: 0.4, rate: [-1, 1].choose if one_in(4)
-    sleep 1
+    sleep 4
   end
 end
 
 live_loop :ambi do
   #stop
+  
+  sync :drums1
+  
   sample ambiring.choose, rate: 0.25, cutoff: rrand(30, 100), amp: 1  if one_in(6)
   sleep 8
 end
@@ -50,6 +60,9 @@ end
 with_fx :reverb do
   #stop
   live_loop :slices do
+    
+    sync :drums1
+    
     with_fx :slicer, phase: [0.25, 0.5, 0.75, 1].choose do
       synth synthring.choose, note: noteringsynth.choose, release: 8, cutoff: rrand(30, 100), amp: 0.3
     end
